e9240a1f2265481b8042aacc3324f2b9c2d1eb01
archie426 <archiestride42@gmail.com>
2020-01-12 17:17:29 +0000

    Add all the files

---
+++ ItemUpdater/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace ItemUpdater
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please make sure you are inside a directory with ONLY the items you want to update");
+            Console.WriteLine("Grabbing files, hold on");
+            string address = Assembly.GetExecutingAssembly().Location.Replace("ItemUpdater.dll", "");
+            foreach (var file in DirSearch(address).Where(file => file.Contains(".dat") && !file.Contains("English")))
+            {
+                _filesToChange.Add(file);
+            }
+            Console.WriteLine("Found " + _filesToChange.Count + " files to change.");
+            int completedFiles = 0;
+            foreach (String file in _filesToChange)
+            {
+                File.AppendAllText(file,"\nExclude_From_Master_Bundle");
+                completedFiles++;
+                Console.WriteLine("Completed " + completedFiles + "/" + _filesToChange.Count);
+            }
+            
+            Console.WriteLine("Done!");
+            
+        }
+        
+        private static List<String> _filesToChange = new List<string>();
+        
+        
+        //Not my code below, thanks to whoever did it.
+        private static List<String> DirSearch(string sDir)
+        {
+            List<String> files = new List<String>();
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir))
+                {
+                    files.Add(f);
+                }
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    files.AddRange(DirSearch(d));
+                }
+            }
+            catch (Exception excpt)
+            {
+                Console.WriteLine(excpt);
+            }
+        
+            return files;
+        }
+        
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Item Updater for Unturned 2017
+
+Put everything beginning with ItemUpdater (except the .sln file and the .csproj file) into your items folder. If you get some sort of error or CMD closes immediately it means you've missed something out, 
+OR
+* You haven't installed .NET SDK 2.1 (Required)
+* dotnet hasn't been set as an Environment Variable (should be automatically done in the installation)
+
+Some files may need rebundling, which this doesn't do. Tested with UNTURNED RP Bundle (A somewhat 950 files that needed changing, so you can see the need) and it works.
