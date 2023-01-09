18e85a6f65b6abc09e75b951c7b2dbbb9acddb74
archie426 <archiestride42@gmail.com>
2023-01-09 12:58:43 +0000

    Update to .NET 7, convert to using NinkyNonk.Shared

--- ItemUpdater/Program.cs
+++ ItemUpdater/Program.cs
@@ -4,73 +4,69 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using NinkyNonk.Shared.Environment;
 
-namespace ItemUpdater
+namespace ItemUpdater;
+
+public static class Program
 {
-    public static class Program
+    public static async Task Main(string[] args)
     {
-        public static void Main(string[] args) => MainAsync(args).GetAwaiter().GetResult();
+        Project.LoggingProxy.LogProgramInfo();
+        Project.LoggingProxy.Log("Please make sure you are inside a directory with ONLY the items you want to update");
+        await Task.Delay(3000);
+        Project.LoggingProxy.LogInfo("Grabbing files...");
+            
+        string address = Assembly.GetExecutingAssembly().Location;
+        address = address.Replace(address.Contains("exe") ? "ItemUpdater.exe" : "ItemUpdater.dll", "");
+            
+        Project.LoggingProxy.LogUpdate("Address: " + address);
 
-        private static async Task MainAsync(string[] args)
+        foreach (var file in (await DirSearch(address)).Where(ShouldEdit))
+            FilesToChange.Add(file);
+            
+        Project.LoggingProxy.LogUpdate("Found " + FilesToChange.Count + " files to change.");
+        int completedFiles = 0;
+        foreach (string file in FilesToChange)
         {
-            Console.WriteLine("Please make sure you are inside a directory with ONLY the items you want to update");
-            await Task.Delay(3000);
-            Console.WriteLine("Grabbing files, hold on");
-            
-            string address = Assembly.GetExecutingAssembly().Location;
-            address = address.Replace(address.Contains("exe") ? "ItemUpdater.exe" : "ItemUpdater.dll", "");
-            
-            Console.WriteLine("Address: " + address);
-
-            foreach (var file in (await DirSearch(address)).Where(ShouldEdit))
-                FilesToChange.Add(file);
-            
-            Console.WriteLine("Found " + FilesToChange.Count + " files to change.");
-            int completedFiles = 0;
-            foreach (string file in FilesToChange)
-            {
-                await File.AppendAllTextAsync(file,"\nExclude_From_Master_Bundle");
-                completedFiles++;
-                Console.WriteLine("Completed " + completedFiles + "/" + FilesToChange.Count);
-            }
-            
-            Console.WriteLine("Done!");
-            await Task.Delay(100000);
-
+            await File.AppendAllTextAsync(file,"\nExclude_From_Master_Bundle");
+            completedFiles++;
+            Project.LoggingProxy.LogUpdate("Completed " + completedFiles + "/" + FilesToChange.Count);
         }
-        
-        private static readonly List<string> FilesToChange = new List<string>();
-
-        public static bool ShouldEdit(string fileLocation)
-        {
-            bool fileCheck = fileLocation.Contains(".dat") && !fileLocation.Contains("English");
-            if (!fileCheck)
-                return false;
-            string fileText = File.ReadAllText(fileLocation).ToLower();
-            return !fileText.Contains("exclude_from_master_bundle") && !fileText.Contains("npc") &&
-                   !fileText.Contains("vendor");
-        } 
-        
-        
-        //Not my code below, thanks to whoever did it.
-        private static async Task<List<string>> DirSearch(string sDir)
-        {
-            List<string> files = new List<string>();
-            try
-            {
-                files.AddRange(Directory.GetFiles(sDir));
-                foreach (string d in Directory.GetDirectories(sDir))
-                    files.AddRange(await DirSearch(d));
-                
-            }
-            catch (Exception excpt)
-            {
-                Console.WriteLine(excpt);
-            }
-        
-            return files;
-        }
-        
-        
+            
+        Project.LoggingProxy.LogSuccess("Done!");
+        Console.ReadKey();
     }
+        
+    private static readonly List<string> FilesToChange = new();
+
+    private static bool ShouldEdit(string fileLocation)
+    {
+        bool fileCheck = fileLocation.Contains(".dat") && !fileLocation.Contains("English");
+        if (!fileCheck)
+            return false;
+        string fileText = File.ReadAllText(fileLocation).ToLower();
+        return !fileText.Contains("exclude_from_master_bundle") && !fileText.Contains("npc") &&
+               !fileText.Contains("vendor");
+    } 
+    
+    //Not my code below, thanks to whoever did it.
+    private static async Task<List<string>> DirSearch(string sDir)
+    {
+        List<string> files = new List<string>();
+        try
+        {
+            files.AddRange(Directory.GetFiles(sDir));
+            foreach (string d in Directory.GetDirectories(sDir))
+                files.AddRange(await DirSearch(d));
+                
+        }
+        catch (Exception excpt)
+        {
+            Project.LoggingProxy.LogError(excpt.Message);
+        }
+        
+        return files;
+    }
+    
 }
--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# Item Updater for Unturned 2017
+# Item Updater for Unturned Unity 2017
 
 Put everything beginning with ItemUpdater (except the .sln file and the .csproj file) into your items folder and run the .bat script. If you get some sort of error or CMD closes immediately it means you've missed something out, 
 OR
-* You haven't installed .NET SDK 2.1 (Required)
-* dotnet hasn't been set as an Environment Variable (should be automatically done in the installation)
 
 Some files may need rebundling, which this doesn't do. Tested with UNTURNED RP Bundle (A somewhat 950 files that needed changing, so you can see the need) and it works.
 
 I haven't tried this with Objects or Effects, as I've never done anything with them before I have no idea. 
 
-Pro tip: Make a backup of your files before doing something that your not sure about
+Pro tip: Make a backup of your files before doing something that you're not sure about
